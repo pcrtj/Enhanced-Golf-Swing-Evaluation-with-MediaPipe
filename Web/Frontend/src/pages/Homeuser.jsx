947f46054bd01d00047f7c8e8272dd0bfba6e053
unknown <pacharatemjai@gmail.com>
2024-08-23 19:36:55 +0800

    update home page

--- Web/Frontend/src/pages/Homeuser.jsx
+++ Web/Frontend/src/pages/Homeuser.jsx
@@ -1,13 +1,46 @@
-import React from "react";
-import "../css/HomePage.css";
+import React from 'react';
+import '../css/HomePage.css';
 
-function Homeuser() {
+const HomePage = () => {
   return (
-    <body className="home-page">
-      <div>Home User</div>
-    </body>
+    <div>
+      <div className="homepage">
+        <div className="content">
+          <h1 className='headerhome'>Enhanced Golf Swing Evaluation <mark>with MediaPipe</mark></h1>
+            <p className='content'>
+              Wireless Ad-Hoc and Sensor Networks Laboratory, <br></br>Department of Computer Science and Information Engineering, <br></br>National Central University, Taiwan
+            </p>
+            <button className="getstarted">Get Started</button>
+        </div>
+        <div className="image-placeholder">
+          <img className="h-image" src="/images/landscapeBG.jpg" alt="Header-image"></img>
+        </div>
+      </div>
+    
+    <section className="section2">
+    <h2 className="section-title">Introduction</h2>
+      <p className="section-text">
+        Nature is a vast and diverse realm, encompassing a wide range of ecosystems, landscapes, and living organisms. This project aims to explore the beauty and complexity of the natural world, highlighting its intricate balance and the importance of preserving its delicate equilibrium.
+      </p>
+    </section>
+
+    <section className="section3">
+    <h2 className="section-title">Abstract</h2>
+      <p className="section-text">
+        Nature is a vast and diverse realm, encompassing a wide range of ecosystems, landscapes, and living organisms. This project aims to explore the beauty and complexity of the natural world, highlighting its intricate balance and the importance of preserving its delicate equilibrium.
+      </p>
+    </section>
+
+    <section className="section4">
+      <h2 className="section-title">Objective</h2>
+      <p className="section-text">
+        The primary objective of this project is to capture the breathtaking beauty of nature through artistic expressions and scientific understanding, promoting awareness and conservation efforts to sustain the environment for future generations.
+      </p>
+    </section>
+    </div>
+    
     
   );
-}
+};
 
-export default Homeuser
+export default HomePage;
